da221fa16649d38b13e09decd0da6fb6cc074faf
Abdulloxon Roziyev <roziyevabdulloxon8@gmail.com>
2025-12-14 20:30:45 +0500

    tayyor

--- config/settings.py
+++ config/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
-
+STATIC_ROOT = "var/www/foodapi/staticfiles"
 
 AUTH_USER_MODEL = 'food.CustomUser'
 
